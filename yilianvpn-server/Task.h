a421086ab712b427f32600e79d6e1c4a375c61e4
管成祖 <1351606745@msn.cn>
2026-06-16 18:24:14 +0800

    Add files via upload

---
+++ yilianvpn-server/Task.h
@@ -0,0 +1,133 @@
+#ifndef TASK_H
+#define TASK_H
+#include <iostream>
+#include <vector>
+using namespace std;
+
+std::vector<void *> tasks;
+struct taskInfo
+{
+	int size;
+	int active;
+	int sleep;
+}; 
+
+class Task
+{
+public:
+	//是否退出
+	bool isQuit;
+	int tick;
+	int loopCount;
+
+	Task()
+	{
+		UID++;
+		id = UID;
+		isQuit = false;
+		tick = 500;
+		loopCount = 1;
+		create_task(this);
+	}
+
+	~Task()
+	{
+		for (int i = 0; i < tasks.size(); i++)
+		{
+			Task *task = (Task *)tasks[i];
+			if(*task == *this)
+			{
+				tasks.erase(tasks.begin() + i);
+				break;
+			}
+		}
+	}
+
+	void quit()
+	{
+		isQuit = true;
+	}
+
+	// 虚构函数loop
+	virtual bool loop()
+	{
+		return false;
+	}
+	
+	// 虚构函数clearJob
+	virtual bool clearJob()
+	{
+		return false;
+	}
+
+	bool operator==(const Task &task)
+	{
+		return id == task.id;
+	}
+
+	long getId()
+	{
+		return id;
+	}
+
+	void create_task(Task *task)
+	{
+		tasks.push_back(task);
+	}
+
+private:
+	// 任务id
+	long id;
+	static long UID;
+};
+
+long Task::UID = 0;
+
+struct taskInfo task_loop()
+{
+	struct taskInfo info = {0};
+	info.active = tasks.size();
+	for(int i = 0; i < tasks.size(); i++)
+	{
+		Task *task = (Task *)tasks[i];
+		if(task->isQuit)
+		{
+			task->clearJob();
+			tasks.erase(tasks.begin() + i);
+			i--;
+			info.active--;
+			continue;
+		} 
+		if(task->tick > 0)
+		{
+			task->tick--;
+			info.sleep++;
+			continue;
+		}
+		int lcount = task->loopCount; 
+		for(int j = 0; j < lcount; j++)
+		{
+			bool exit = task->loop();
+			if(exit)
+			{
+				task->isQuit = true;
+				task->clearJob();
+				tasks.erase(tasks.begin() + i);
+				i--; 
+				j = lcount;
+				info.active--;
+			}
+			if(task->tick > 0)
+			{
+				j = lcount;
+				info.sleep++;
+			}
+		}
+	}
+	info.size = tasks.size();
+	return info;
+}
+
+#endif
+
+
